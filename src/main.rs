94c8384920ba8242c323de70020852be031eaa60
DanceMore <dancemore@protonmail.com>
2023-10-14 22:02:10 -0700

    shader?

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,211 @@ use bevy::app::AppExit;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy_ascii_terminal::{code_page_437, prelude::*};
-use rand::prelude::ThreadRng;
-use rand::Rng;
+//use rand::prelude::ThreadRng;
+//use rand::Rng;
 
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use bevy_fundsp::prelude::*;
-
 
 fn main() {
     App::new()
         .init_resource::<Pause>()
+        .init_resource::<UiState>()
+        //.init_resource::<DspData>()
+        .insert_resource(DspManager::default())
         .insert_resource(CurrentSpamFunction { index: 0 }) // Initialize with your default function
+	//.insert_resource(MyShaderData {
+	//	r: 0.0,
+	//	g: 0.0,
+	//	b: 0.0,
+	//})
         .add_plugins((
             DefaultPlugins,
             TerminalPlugin,
             LogDiagnosticsPlugin::default(),
             FrameTimeDiagnosticsPlugin::default(),
         ))
+        .add_plugins(MaterialPlugin::<CustomMaterial>::default())
         .add_plugins(DspPlugin::default())
-        .add_dsp_source(white_noise, SourceType::Dynamic)
-        .add_dsp_source(generate_sine_wave, SourceType::Dynamic)
+        .add_plugins(EguiPlugin)
+        .add_plugins(PianoPlugin)
         .insert_resource(ClearColor(Color::BLACK))
+	.add_asset::<CustomMaterial>()
         .add_systems(Startup, setup)
-        .add_systems(Update, spam_terminal)
-	.add_systems(Update, quit_on_escape)
-	.add_systems(PostStartup, play_sine_wave)
+        //.add_systems(Update, spam_terminal)
+        .add_systems(Update, quit_on_escape)
+        .add_systems(Update, ui_example_system)
+        //.add_systems(Update, sync_data)
+        //.add_systems(Update, update_dsp_data)
         .run();
+}
+
+//fn sync_data(ui_state: ResMut<UiState>,
+//             mut shader_data: ResMut<MyShaderData>,
+//) {
+//	shader_data.r = ui_state.r.clone() as f32;
+//	shader_data.g = ui_state.g.clone() as f32;
+//	shader_data.b = ui_state.b.clone() as f32;
+//
+//	//println!("[-] {:?}", shader_data);
+//}
+
+use bevy::{
+    pbr::{MaterialPipeline, MaterialPipelineKey},
+    reflect::{TypePath, TypeUuid},
+    render::{
+        mesh::MeshVertexBufferLayout,
+        render_resource::{
+            AsBindGroup, RenderPipelineDescriptor, ShaderRef, SpecializedMeshPipelineError,
+        },
+    },
+};
+
+
+use bevy::asset::LoadState;
+
+#[derive(Reflect, TypeUuid, AsBindGroup, Debug, Clone)]
+#[uuid = "b8e1724f-3311-4d4f-a5ad-e167b78436e0"]
+struct CustomMaterial {}
+
+impl Material for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        //"shaders/animate_shader.wgsl".into()
+        "shaders/haxor.wgsl".into()
+    }
+
+    //fn vertex_shader() -> ShaderRef {
+    //    "shaders/haxor.vert".into()
+    //}
+
+    //fn fragment_shader() -> ShaderRef {
+    //    //"shaders/custom_material.frag".into()
+    //    "shaders/haxor.frag".into()
+    //}
+
+    //// Bevy assumes by default that vertex shaders use the "vertex" entry point
+    //// and fragment shaders use the "fragment" entry point (for WGSL shaders).
+    //// GLSL uses "main" as the entry point, so we must override the defaults here
+    //fn specialize(
+    //    _pipeline: &MaterialPipeline<Self>,
+    //    descriptor: &mut RenderPipelineDescriptor,
+    //    _layout: &MeshVertexBufferLayout,
+    //    _key: MaterialPipelineKey<Self>,
+    //) -> Result<(), SpecializedMeshPipelineError> {
+    //    descriptor.vertex.entry_point = "main".into();
+    //    descriptor.fragment.as_mut().unwrap().entry_point = "main".into();
+    //    Ok(())
+    //}
+}
+
+// This is the struct that will be passed to your shader
+#[repr(C)]
+#[derive(AsBindGroup,Component)]
+struct MyShaderData {
+  #[uniform(0)]
+  r: f32,
+  g: f32,
+  b: f32,
+  _pad: f32,
+}
+
+impl Default for MyShaderData {
+    fn default() -> Self {
+        MyShaderData {
+            r: 0.1,
+	    g: 0.0,
+	    b: 0.0,
+	    _pad: 0.0,
+
+        }
+    }
+}
+
+//#[derive(Resource, Default)]
+//struct MyShaderData {
+//    some_value: f32,
+//    // Add more fields as needed
+//}
+
+// set up a simple 3D scene
+fn setup(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	//mut materials: ResMut<Assets<CustomMaterial>>,
+	mut s_materials: ResMut<Assets<StandardMaterial>>,
+	mut c_materials: ResMut<Assets<CustomMaterial>>,
+	    asset_server: Res<AssetServer>,
+	) {
+
+    // light
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 5.0, 4.0),
+        ..default()
+    });
+
+   // cube1
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::from(bevy::prelude::shape::Cube { size: 1.0 })),
+        transform: Transform::from_xyz(2.0, 0.5, -1.0),
+	material: s_materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        ..default()
+    });
+
+
+   // cube2, shader boogaloo
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::from(bevy::prelude::shape::Cube { size: 1.0 })),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+	//material: s_materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        material: c_materials.add(CustomMaterial {} ),
+        ..default()
+    });
+
+
+    println!("[-] drawing camera");
+	// camera
+	commands.spawn(Camera3dBundle {
+		transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+		..default()
+	});
+
+}
+
+fn ui_example_system(time: Res<Time>, mut contexts: EguiContexts,     mut ui_state: ResMut<UiState>,
+		     dsp_manager: Res<DspManager>,
+		     //mut shader_data: ResMut<MyShaderData>,
+		     ) {
+
+    egui::SidePanel::left("side_panel")
+        .default_width(200.0)
+        .show(contexts.ctx_mut(), |ui| {
+            ui.heading("Side Panel");
+
+            ui.add(egui::Slider::new(&mut ui_state.value, 20.0..=24000.0).text("value"));
+            if ui.button("Increment").clicked() {
+                ui_state.value += 1.0;
+            }
+
+            ui.add(egui::Slider::new(&mut ui_state.shader_data.r, 0.0..=100000.0).text("value"));
+            ui.add(egui::Slider::new(&mut ui_state.shader_data.g, 0.0..=1.0).text("value"));
+            ui.add(egui::Slider::new(&mut ui_state.shader_data.b, 0.0..=1.0).text("value"));
+
+	    //example_plot(ui, &time, &[1.0,2.0,3.0]);
+	    //example_plot(ui, &time);
+	    //println!("[+] drew boring plot");
+	//    plot_dsp(ui, &time, dsp_manager);
+        });
+
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
+}
+
+#[derive(Resource, Default)]
+struct UiState {
+    label: String,
+    value: f32,
+    shader_data: MyShaderData,
 }
 
 #[derive(Resource, Default)]
@@ -37,143 +217,18 @@ struct CurrentSpamFunction {
     index: usize,
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn((
-        TerminalBundle::new()
-            .with_size([80, 50])
-            .with_border(Border::single_line()),
-        AutoCamera,
-    ));
-}
+//fn setup(mut commands: Commands,
+//) {
+//    commands.spawn((
+//        TerminalBundle::new()
+//            .with_size([80, 50])
+//            .with_border(Border::single_line()),
+//        AutoCamera,
+//    ));
+//}
 
 fn setup_audio(commands: &mut Commands) {
     commands.insert_resource(DspManager::default());
-}
-
-fn draw_helptext(mut q: Query<&mut Terminal>) {
-    for mut term in q.iter_mut() {
-		    let top = term.side_index(bevy_ascii_terminal::Side::Top) as i32;
-		    term.clear_box([0, top], [30, 1]);
-		    term.put_string([0, top], "Press space to pause");
-		    term.clear_box([0, top-1], [30, 1]);
-		    term.put_string([0, top-1], "Left / Right to change effect");
-	    }
-}
-
-fn rand_color(rng: &mut ThreadRng) -> Color {
-    let r: f32 = rng.gen_range(0.0..=1.0);
-    let g: f32 = rng.gen_range(0.0..=1.0);
-    let b: f32 = rng.gen_range(0.0..=1.0);
-    Color::rgb(r, g, b)
-}
-
-fn spam_terminal_a(_time: Res<Time>, mut q: Query<&mut Terminal>) {
-    let mut rng = rand::thread_rng();
-    for mut term in q.iter_mut() {
-        for t in term.iter_mut() {
-            let index = rng.gen_range(0..=255) as u8;
-            let glyph = code_page_437::index_to_glyph(index);
-            let fg = rand_color(&mut rng);
-            let bg = rand_color(&mut rng);
-
-            *t = Tile {
-                glyph,
-                fg_color: fg,
-                bg_color: bg,
-            }
-        }
-    }
-
-    draw_helptext(q);
-}
-
-fn spam_terminal_b(_time: Res<Time>, mut q: Query<&mut Terminal>) {
-    let mut rng = rand::thread_rng();
-    let color_palette = [
-        Color::RED,
-        Color::GREEN,
-        Color::BLUE,
-        Color::YELLOW,
-        Color::CYAN,
-        Color::WHITE,
-    ];
-
-    for (mut term, color) in q.iter_mut().zip(color_palette.iter()) {
-        let mut current_color_index = 0;
-
-        for t in term.iter_mut() {
-            let index = rng.gen_range(0..=255) as u8;
-            let glyph = code_page_437::index_to_glyph(index);
-            let fg = color_palette[current_color_index];
-            let bg = Color::BLACK; // Set background color to black
-
-            *t = Tile {
-                glyph,
-                fg_color: fg,
-                bg_color: bg,
-            };
-
-            current_color_index = (current_color_index + 1) % color_palette.len();
-        }
-
-    }
-    draw_helptext(q);
-}
-
-fn spam_terminal_c(time: Res<Time>, mut q: Query<&mut Terminal>) {
-    let mut rng = rand::thread_rng();
-    const SHIFT_MULT : f32 = 60.0;
-
-    for mut term in q.iter_mut() {
-        for t in term.iter_mut() {
-            let index = rng.gen_range(0..=255) as u8;
-            let glyph = code_page_437::index_to_glyph(index);
-	    //let glyph = random_hiragana_char(&mut rng);
-            let elapsed_time = time.elapsed_seconds();
-            let hue = (elapsed_time * SHIFT_MULT) % 360.0;
-            let bg = Color::hsl(hue, 1.0, 0.5);
-            let fg = Color::BLACK;
-
-            *t = Tile {
-                glyph,
-                fg_color: fg,
-                bg_color: bg,
-            }
-        }
-    }
-
-    draw_helptext(q);
-}
-
-
-type SpamFunction = fn(time: Res<Time>, Query<&mut Terminal>);
-
-fn spam_terminal(
-    keys: Res<Input<KeyCode>>,
-    mut pause: ResMut<Pause>,
-    q: Query<&mut Terminal>,
-    mut spammer: ResMut<CurrentSpamFunction>,
-    time: Res<Time>, // Add this line to include Time resource
-) {
-    let spam_functions: Vec<SpamFunction> = vec![spam_terminal_a, spam_terminal_b, spam_terminal_c];
-
-    if keys.just_pressed(KeyCode::Space) {
-        pause.0 = !pause.0;
-    }
-
-    if pause.0 {
-        return;
-    }
-
-    //spam_terminal_b(q);
-
-    if keys.just_pressed(KeyCode::Right) {
-        spammer.index = (spammer.index + 1) % spam_functions.len();
-    } else if keys.just_pressed(KeyCode::Left) {
-        spammer.index = (spammer.index + spam_functions.len() - 1) % spam_functions.len();
-    }
-
-    spam_functions[spammer.index](time, q);
 }
 
 fn quit_on_escape(input: Res<Input<KeyCode>>, mut exit_events: ResMut<Events<AppExit>>) {
@@ -184,68 +239,131 @@ fn quit_on_escape(input: Res<Input<KeyCode>>, mut exit_events: ResMut<Events<App
     }
 }
 
-//fn random_latin_char(rng: &mut impl Rng) -> char {
-//    // Define the Unicode range you want to select characters from
-//    // For example, this generates random characters from the basic Latin block
-//    let start = 0x0020 as u32; // Start of the basic Latin block
-//    let end = 0x007E as u32;   // End of the basic Latin block
-//
-//    // Generate a random Unicode code point within the specified range
-//    let code_point = rng.gen_range(start..=end);
-//
-//    // Convert the code point to a character
-//    char::from_u32(code_point).unwrap_or('?') // Handle invalid code points gracefully
-//}
+use uuid::Uuid;
 
-// bevy_ascii_term can't do unicode, only ascii (duh)
-//fn random_hiragana_char(rng: &mut impl Rng) -> char {
-//    // Define the Unicode range for Hiragana characters
-//    let start = 0x3040 as u32;
-//    let end = 0x309F as u32;
-//
-//    // Generate a random Unicode code point within the specified range
-//    let code_point = rng.gen_range(start..=end);
-//
-//    // Convert the code point to a character
-//    char::from_u32(code_point).unwrap_or('?') // Handle invalid code points gracefully
-//}
+struct PianoPlugin;
 
-fn white_noise() -> impl AudioUnit32 {
-    white() >> split::<U2>() * 0.2
+struct PianoDsp<F>(F);
+
+impl<T: AudioUnit32 + 'static, F: Send + Sync + 'static + Fn() -> T> DspGraph for PianoDsp<F> {
+    fn id(&self) -> Uuid {
+        Uuid::from_u128(0xa1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8u128)
+    }
+
+    fn generate_graph(&self) -> Box<dyn AudioUnit32> {
+        Box::new((self.0)())
+    }
 }
 
-fn play_noise(
+#[derive(Debug, Resource)]
+struct PianoId(Uuid);
+
+#[derive(Resource)]
+struct PitchVar(Shared<f32>);
+
+impl PitchVar {
+    fn set_pitch(&self, pitch: Pitch) {
+        self.0.set_value(pitch.into());
+    }
+}
+
+impl PitchVar {
+    fn set_freq(&self, freq: f32) {
+        self.0.set_value(freq);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Pitch {
+    C,
+    Cs,
+    D,
+    Ds,
+    E,
+    F,
+    Fs,
+    G,
+    Gs,
+    A,
+    As,
+    B,
+}
+
+impl Pitch {
+    fn to_f32(self) -> f32 {
+        match self {
+            Pitch::C => 261.626,
+            Pitch::Cs => 277.183,
+            Pitch::D => 293.665,
+            Pitch::Ds => 311.127,
+            Pitch::E => 329.628,
+            Pitch::F => 349.228,
+            Pitch::Fs => 369.994,
+            Pitch::G => 391.995,
+            Pitch::Gs => 415.305,
+            Pitch::A => 440.0,
+            Pitch::As => 466.164,
+            Pitch::B => 493.883,
+        }
+    }
+}
+
+impl From<Pitch> for f32 {
+    fn from(pitch: Pitch) -> Self {
+        pitch.to_f32()
+    }
+}
+
+impl Plugin for PianoPlugin {
+    fn build(&self, app: &mut App) {
+        let pitch = shared(Pitch::C.into());
+        let pitch2 = pitch.clone();
+
+        let piano = move || var(&pitch2) >> sine() >> split::<U2>() * 0.2;
+        let piano_dsp = PianoDsp(piano.clone());
+        let piano_id = piano_dsp.id();
+
+        app.add_dsp_source(piano_dsp, SourceType::Dynamic)
+            .insert_resource(PitchVar(pitch))
+            .insert_resource(PianoId(piano_id))
+            .add_systems(Update, switch_key)
+            .add_systems(PostStartup, play_piano);
+    }
+}
+
+fn switch_key(input: Res<Input<KeyCode>>, pitch_var: Res<PitchVar>, ui_state: Res<UiState>) {
+    pitch_var.set_freq(ui_state.value);
+
+    let keypress = |keycode, pitch| {
+        if input.just_pressed(keycode) {
+            pitch_var.set_pitch(pitch)
+        }
+    };
+
+    keypress(KeyCode::A, Pitch::C);
+    keypress(KeyCode::W, Pitch::Cs);
+    keypress(KeyCode::S, Pitch::D);
+    keypress(KeyCode::E, Pitch::Ds);
+    keypress(KeyCode::D, Pitch::E);
+    keypress(KeyCode::F, Pitch::F);
+    keypress(KeyCode::T, Pitch::Fs);
+    keypress(KeyCode::G, Pitch::G);
+    keypress(KeyCode::Y, Pitch::Gs);
+    keypress(KeyCode::H, Pitch::A);
+    keypress(KeyCode::U, Pitch::As);
+    keypress(KeyCode::J, Pitch::B);
+}
+
+fn play_piano(
     mut commands: Commands,
     mut assets: ResMut<Assets<DspSource>>,
     dsp_manager: Res<DspManager>,
+    piano_id: Res<PianoId>,
 ) {
     let source = assets.add(
         dsp_manager
-            .get_graph(white_noise)
-            .unwrap_or_else(|| panic!("DSP source not found!"))
-            .clone(),
-    );
-    commands.spawn(AudioSourceBundle {
-        source,
-        ..default()
-    });
-}
-
-fn generate_sine_wave() -> impl AudioUnit32 {
-    // Generate a sine wave at 440 Hz (A4)
-    sine_hz(440.0) * 0.2
-}
-
-fn play_sine_wave(
-    mut commands: Commands,
-    mut assets: ResMut<Assets<DspSource>>,
-    dsp_manager: Res<DspManager>,
-) {
-    let source = assets.add(
-        dsp_manager
-            .get_graph(generate_sine_wave)
-            .unwrap_or_else(|| panic!("DSP source not found!"))
-            .clone(),
+            .get_graph_by_id(&piano_id.0)
+            .unwrap_or_else(|| panic!("DSP source not found!")),
     );
     commands.spawn(AudioSourceBundle {
         source,
